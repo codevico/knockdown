620281ba977dbabed73748f79a0890daf50e5a61
Vico <1497413+codevico@users.noreply.github.com>
2020-08-18 14:51:41 +0200

    was broken, now works again

--- knockdown-ig.js
+++ knockdown-ig.js
@@ -5,7 +5,18 @@ mousePosition.y = mouseMoveEvent.pageY;
 });
 $(document).on('keydown', function(e) {
 	if(e.key.toLowerCase() == 'q' && e.ctrlKey) {
-		var video = $(document.elementFromPoint(mousePosition.x, mousePosition.y)).parent().parent().find('video source');
-		if(typeof video.attr('src') !== 'undefined') window.open(video.attr('src'));
+		console.log('ctrl+q pressed');
+		var video = $(document.elementFromPoint(mousePosition.x, mousePosition.y)).closest('article').find('video');
+		if(video.length==0) {
+			console.log('couldnt find article, finding section');
+			video = $(document.elementFromPoint(mousePosition.x, mousePosition.y)).closest('section').find('video source');
+		}
+		console.log(video.length);
+		if(typeof video.attr('src') !== 'undefined') {
+			window.open(video.attr('src'));
+		}
+		else {
+
+		}
 	}
 });
